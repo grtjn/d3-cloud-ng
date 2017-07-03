ae22fb3de3e55eed6e12a9c935826f2e46287037
Vaclav Pavlicek <vaclav@pavlicek.cz>
2017-07-03 13:45:12 +0200

    add styles to d3-cloud to fit container height and width and fix problem with ignored words

--- src/d3-cloud.controller.js
+++ src/d3-cloud.controller.js
@@ -8,47 +8,57 @@
   d3CloudController.$inject = ['$scope'];
 
   function d3CloudController($scope) {
-    $scope.$watch('words', function(newValue, oldValue) {
-      var i = [];
-      var items = [];
-      var ignore = $scope.ignoreList;
-      var updateflag = 0;
+    $scope.$watch('words', function (newValue, oldValue) {
+      angular.element(document).ready(function () {
+        var i = [];
+        var items = [];
+        var ignore = $scope.ignoreList;
+        var updateflag = 0;
 
-      if (newValue) {
-        for (i = 0; i < newValue.length; i++) {
-          if (ignore.indexOf(newValue[i].name) < 1) {
-            items.push(newValue[i]);
+        if (newValue) {
+          for (i = 0; i < newValue.length; i++) {
+            if (!isIgnored(newValue[i], ignore)) {
+              items.push(newValue[i]);
+            }
           }
-        }
-
-        if ($scope.cloud) {
-          if (oldValue) {
-            if (oldValue.length !== newValue.length) {
-              updateflag = 1;
-            } else {
-              for (i = 0; i < newValue.length; i++) {
-                if (!updateflag & newValue[i].name !== oldValue[i].name & newValue[i].score !== oldValue[i].score) {
-                  updateflag = 1;
+          $scope.words = items;
+          if ($scope.cloud) {
+            if (oldValue) {
+              if (oldValue.length !== newValue.length) {
+                updateflag = 1;
+              } else {
+                for (i = 0; i < newValue.length; i++) {
+                  if (!updateflag & newValue[i].name !== oldValue[i].name & newValue[i].score !== oldValue[i].score) {
+                    updateflag = 1;
+                  }
                 }
               }
+            } else {
+              updateflag = 1;
+            }
+
+            if (updateflag) {
+              // only update if changed
+              $scope.updateCloud(items);
             }
           } else {
-            updateflag = 1;
+            // create from scratch
+            $scope.createCloud(items);
           }
-
-          if (updateflag) {
-            // only update if changed
-            $scope.updateCloud(items);
-          }
-        } else {
-          // create from scratch
-          $scope.createCloud(items);
+        } else if ($scope.cloud) {
+          // flush existing words
+          $scope.updateCloud([]);
         }
-      } else if ($scope.cloud) {
-        // flush existing words
-        $scope.updateCloud([]);
-      }
-
+      });
     }, true);
+    function isIgnored(newValue, ignoredObjects) {
+      var ignored = false;
+      ignoredObjects.forEach(function (ignoredObject) {
+        if (ignoredObject.name === newValue.name && ignoredObject.score === newValue.score && ignoredObject.color === newValue.color) {
+          ignored = true;
+        }
+      });
+      return ignored;
+    }
   }
 })();
