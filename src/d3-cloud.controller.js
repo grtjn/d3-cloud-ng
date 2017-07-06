a4933db084d1e8f231c36d2f57aa169f14c5872a
Vaclav Pavlicek <vaclav@pavlicek.cz>
2017-07-06 12:01:38 +0200

    remove angular.element(document).ready and update styles

--- src/d3-cloud.controller.js
+++ src/d3-cloud.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
   'use strict';
 
@@ -9,44 +9,42 @@
 
   function d3CloudController($scope) {
     $scope.$watch('words', function (newValue, oldValue) {
-      angular.element(document).ready(function () {
-        var i = [];
-        var items = [];
-        var updateflag = 0;
+      var i = [];
+      var items = [];
+      var updateflag = 0;
 
-        if (newValue) {
-          for (var i = 0; i< newValue.length; i++) {
-            if ($scope.filter(newValue[i])) {
-              items.push(newValue[i]);
-            }
+      if (newValue) {
+        for (var i = 0; i < newValue.length; i++) {
+          if ($scope.filter(newValue[i])) {
+            items.push(newValue[i]);
           }
-          if ($scope.cloud) {
-            if (oldValue) {
-              if (oldValue.length !== items.length) {
-                updateflag = 1;
-              } else {
-                for (i = 0; i < items.length; i++) {
-                  if (!updateflag & items[i].name !== oldValue[i].name & items[i].score !== oldValue[i].score) {
-                    updateflag = 1;
-                  }
+        }
+        if ($scope.cloud) {
+          if (oldValue) {
+            if (oldValue.length !== items.length) {
+              updateflag = 1;
+            } else {
+              for (i = 0; i < items.length; i++) {
+                if (!updateflag & items[i].name !== oldValue[i].name & items[i].score !== oldValue[i].score) {
+                  updateflag = 1;
                 }
               }
-            } else {
-              updateflag = 1;
-            }
-            if (updateflag) {
-              // only update if changed
-              $scope.updateCloud(items);
             }
           } else {
-            // create from scratch
-            $scope.createCloud(items);
+            updateflag = 1;
           }
-        } else if ($scope.cloud) {
-          // flush existing words
-          $scope.updateCloud([]);
+          if (updateflag) {
+            // only update if changed
+            $scope.updateCloud(items);
+          }
+        } else {
+          // create from scratch
+          $scope.createCloud(items);
         }
-      });
+      } else if ($scope.cloud) {
+        // flush existing words
+        $scope.updateCloud([]);
+      }
     }, true);
   }
 })();
